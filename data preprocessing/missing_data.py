68629dd6bf3ecb59b75d7cdc612990a3ac586cbf
AKSHAT GUPTA <33572137+akshat-gupta7@users.noreply.github.com>
2019-03-02 16:05:07 +0530

    Update missing_data.py

--- data preprocessing/missing_data.py
+++ data preprocessing/missing_data.py
@@ -1,15 +1,4 @@
-# Data Preprocessing
-
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
-
+# *DO THIS STEP AFTER IMPORTING THE DATASET IN CASE SOME DATA IS MISSING*
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
